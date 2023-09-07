4966021a7a8f5df58acd0373aadcd4bd74147175
Eduard Tanase <eduard.tanase1@gmail.com>
2023-09-07 17:36:12 -0400

    Add folder structure and versioning

--- omega/analyzer/worker/tools/automate.sh
+++ omega/analyzer/worker/tools/automate.sh
@@ -2,12 +2,13 @@
 
 if [ "$#" -lt 2 ]
 then
-  echo "Not enough arguments supplied. Usage: ./automate.sh <user> <repository> [version]"
+  echo "Not enough arguments supplied. Usage: ./automate.sh <user> <repository>"
   exit 1
 fi
 
-git clone https://github.com/$1/$2
+git clone https://github.com/$1/$2 || exit 1
+VERSION=$(cd ./$2 && git rev-parse --short HEAD && cd ..)
 tar -czf $2.tar.gz $2
 mkdir /opt/local_source/
-mv $2.tar.gz /opt/local_source/
-/opt/toolshed/runtools.sh pkg:github/$2@${3:-0}?local=true
+mv $2.tar.gz /opt/local_source/ || exit 1
+/opt/toolshed/runtools.sh pkg:github/$1/$2@$VERSION?local=true || exit 1
