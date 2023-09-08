773b5ce2a16277f505485fba954756d6dbd41bfd
Eduard Tanase <eduard.tanase1@gmail.com>
2023-09-08 16:55:06 -0400

    Remove unnecessary changes and optimize

---
+++ omega/analyzer/worker/tools/github.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]
+then
+  echo "Not enough arguments supplied. Usage: ./github.sh <user> <repository>"
+  exit 1
+fi
+
+mkdir /opt/local_source
+git clone https://github.com/$1/$2 /tmp/$2 || exit 1
+VERSION=$(git --git-dir=/tmp/$2/.git rev-parse --short HEAD)
+tar -czf /opt/local_source/$2.tar.gz /tmp/$2
+/opt/toolshed/runtools.sh pkg:github/$1/$2@$VERSION?local_source=true || exit 1
