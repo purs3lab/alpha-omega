ebb463d689e446dcf4428b51d16ee2a2570d2a2b
Eduard Tanase <eduard.tanase1@gmail.com>
2023-10-07 20:30:55 -0400

    Add workflow

--- omega/analyzer/worker/tools/github.sh
+++ omega/analyzer/worker/tools/github.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]
-then
-  echo "Not enough arguments supplied. Usage: ./github.sh <user> <repository>"
-  exit 1
-fi
-
-mkdir /opt/local_source
-git clone https://github.com/$1/$2 /opt/local_source/$2 || exit 1
-VERSION=$(git --git-dir=/opt/local_source/$2/.git rev-parse --short HEAD)
-/opt/toolshed/runtools.sh pkg:github/$1/$2@$VERSION?local_source=true || exit 1
+/opt/toolshed/runtools.sh pkg:github/target@0?local_source=true || exit 1
