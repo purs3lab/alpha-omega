862daf20f96c5b408951b77bc3784bf697d4544f
Eduard Tanase <eduard.tanase1@gmail.com>
2023-11-12 17:31:04 -0500

    Don't reanalyze repos

--- omega/analyzer/automate.py
+++ omega/analyzer/automate.py
@@ -16,14 +16,34 @@ headers = {
 # GitHub URL pattern for validation, allowing both http and https
 github_url_pattern = re.compile(r'^https?://github\.com/([\w-]+)/([\w-]+)$')
 
+# Function to fetch names of workflow runs that are completed, in progress, or queued
+def fetch_active_runs():
+    url = f'https://api.github.com/repos/purs3lab/alpha-omega/actions/runs'
+    response = requests.get(url, headers=headers)
+    active_runs = set()
+
+    if response.status_code == 200:
+        for run in response.json()['workflow_runs']:
+            if run['status'] in ['completed', 'in_progress', 'queued']:
+                # Assuming the workflow run name includes the repository name
+                name = run['name']
+                active_runs.add(name)
+    
+    return active_runs
+
 # Function to trigger the workflow
-def trigger_workflow(user, repo):
+def trigger_workflow(user, repo, active_runs):
+    # Check if the workflow run is already active for this repo
+    if f'{user}/{repo}' in active_runs:
+        print(f'Skipping {user}/{repo} as it was already queued')
+        return
+
     # URL to trigger the workflow dispatch event
     url = f'https://api.github.com/repos/purs3lab/alpha-omega/actions/workflows/omega_analyzer_pull.yml/dispatches'
 
     # Payload with inputs
     data = {
-        'ref': 'main',  # or the branch you want to dispatch on
+        'ref': 'main',
         'inputs': {
             'user': user,
             'repository': repo
@@ -46,6 +66,9 @@ def validate_and_extract_github_url(url):
     else:
         return None
 
+# Fetch the names of active workflow runs
+active_runs = fetch_active_runs()
+
 # Main loop to run the workflow for each repository URL in the file
 with open(github_repos_file, 'r') as file:
     for repo_url in file:
@@ -54,7 +77,7 @@ with open(github_repos_file, 'r') as file:
             result = validate_and_extract_github_url(repo_url)
             if result:
                 user, repo = result
-                trigger_workflow(user, repo)
+                trigger_workflow(user, repo, active_runs)
             else:
                 print(f'Invalid GitHub URL: {repo_url}')
 
