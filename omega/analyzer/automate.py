2665040fbcc21afc3718c839bbf3e2be32fc3650
Eduard Tanase <eduard.tanase1@gmail.com>
2023-11-12 16:26:48 -0500

    Named runs

--- omega/analyzer/automate.py
+++ omega/analyzer/automate.py
@@ -1,40 +1,63 @@
-import os
-import argparse
+import requests
+import re
 
+# Replace this with your GitHub token
+GITHUB_TOKEN = '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'
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="python ./automate.py",
-        description="Automatically run the Omega Analyzer on a list of repositories"
-    )
-    parser.add_argument("-i", "--input", default="list.txt", help="input file (default list.txt)")
-    parser.add_argument("-f", "--force", action="store_true", help="overwrite previous analysis")
-    args = parser.parse_args()
+# Path to your file containing GitHub repository URLs
+github_repos_file = 'github_repos.txt'
 
-    with open(args.input, "r") as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            line = lines[i]
-            if "github" not in line:
-                print(line + " is not a github repository")
-                with open("failed.txt", "a") as failed_list:
-                    failed_list.write(line + "\n")
-                continue
+# Header for authentication
+headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': f'Bearer {GITHUB_TOKEN}',
+}
 
-            user, repo = line.split("/")[-2:]
+# GitHub URL pattern for validation, allowing both http and https
+github_url_pattern = re.compile(r'^https?://github\.com/([\w-]+)/([\w-]+)$')
 
-            if not args.force and not os.system("test -d ./results/" + user + "/" + repo):
-                print(f'[{i + 1}/{len(lines)}] Skipping {user}/{repo} (Already exists)')
-                continue
+# Function to trigger the workflow
+def trigger_workflow(user, repo):
+    # URL to trigger the workflow dispatch event
+    url = f'https://api.github.com/repos/purs3lab/alpha-omega/actions/workflows/omega_analyzer_pull.yml/dispatches'
 
-            print(f'[{i + 1}/{len(lines)}] Analyzing {user}/{repo}...')
-            rc = os.system(
-                f'docker run --rm -it -v ./results:/opt/export/github openssf/omega-toolshed:latest {user} {repo}'
-            )
-            if rc:
-                print("Analyzer exited with code 1. Exiting script...")
-                return
+    # Payload with inputs
+    data = {
+        'ref': 'main',  # or the branch you want to dispatch on
+        'inputs': {
+            'user': user,
+            'repository': repo
+        }
+    }
 
+    # Post request to trigger the workflow
+    response = requests.post(url, headers=headers, json=data)
+    
+    if response.status_code == 204:
+        print(f'Workflow dispatched for {user}/{repo} successfully.')
+    else:
+        print(f'Failed to dispatch workflow for {user}/{repo}. Response: {response.status_code} - {response.text}')
 
-if __name__ == "__main__":
-    main()
+# Function to validate and extract user/repo from the GitHub URL
+def validate_and_extract_github_url(url):
+    match = github_url_pattern.match(url)
+    if match:
+        return match.groups()
+    else:
+        return None
+
+# Main loop to run the workflow for each repository URL in the file
+with open(github_repos_file, 'r') as file:
+    for repo_url in file:
+        repo_url = repo_url.strip()
+        if repo_url:
+            result = validate_and_extract_github_url(repo_url)
+            if result:
+                user, repo = result
+                trigger_workflow(user, repo)
+            else:
+                print(f'Invalid GitHub URL: {repo_url}')
+
+# Ensure you replace 'YOUR_GITHUB_TOKEN' with your actual GitHub Personal Access Token
+# Also ensure that the 'github_repos.txt' file exists and contains valid GitHub repository URLs.
+
