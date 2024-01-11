03afee04fbe7d54d6f13eadd0a4f1c5f69ddc889
Mingjie Shen <shen497@purdue.edu>
2024-01-11 18:14:43 +0000

    update

--- omega/analyzer/automate.py
+++ omega/analyzer/automate.py
@@ -2,9 +2,11 @@ import requests
 import re
 import argparse
 import time
+import os
 
 GITHUB_TOKEN = 'YOUR_GITHUB_TOKEN'
 github_repos_file = 'github_repos.txt'
+results_dir = '/YOUR/DIR/workflow_results'
 
 headers = {
     'Accept': 'application/vnd.github+json',
@@ -18,17 +20,35 @@ parser.add_argument("-l", "--limit", type=int, default=0, help="Limit the number
 args = parser.parse_args()
 
 # Function to wait for workflow completion
-def wait_for_workflow_completion(user, repo):
-    print(f'Waiting for workflow completion for {user}/{repo}...')
-    while True:
-        url = 'https://api.github.com/repos/purs3lab/alpha-omega/actions/runs'
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            runs = response.json()['workflow_runs']
-            for run in runs:
-                if run['status'] == 'completed' and run['name'] == f'{user}/{repo}':
-                    return run['id']
-        time.sleep(60)  # Wait for 60 seconds before checking again
+#def wait_for_workflow_completion(user, repo):
+#    print(f'Waiting for workflow completion for {user}/{repo}...')
+#    while True:
+#        url = 'https://api.github.com/repos/purs3lab/alpha-omega/actions/runs'
+#        response = requests.get(url, headers=headers)
+#        if response.status_code == 200:
+#            runs = response.json()['workflow_runs']
+#            for run in runs:
+#                if run['status'] == 'completed' and run['name'] == f'{user}/{repo}':
+#                    return run['id']
+#        time.sleep(60)  # Wait for 60 seconds before checking again
+
+def num_of_queued_wf_runs():
+    url = 'https://api.github.com/repos/purs3lab/alpha-omega/actions/runs'
+    response = requests.get(url, headers=headers, params={'status': 'queued'})
+    if response.status_code == 200:
+        return response.json()['total_count']
+    return -1
+
+# Download artifacts of successful workflow runs
+def download_results():
+    url = 'https://api.github.com/repos/purs3lab/alpha-omega/actions/runs'
+    response = requests.get(url, headers=headers, params={'status': 'success'})
+    if response.status_code == 200:
+        runs = response.json()['workflow_runs']
+        for run in runs:
+            run_id = run['id']
+            download_artifact(run_id)
+            delete_workflow_run(run_id)
 
 # Function to download workflow artifact
 def download_artifact(run_id):
@@ -44,18 +64,34 @@ def download_artifact(run_id):
         download_url = artifact['archive_download_url']
         print(f'Downloading artifact from {download_url}')
         download_response = requests.get(download_url, headers=headers)
-        with open(f'/workdisk/POSSE/workflow_results/{artifact["name"]}.zip', 'wb') as file:
+        with open(f'{results_dir}/{artifact["name"]}.zip', 'wb') as file:
             file.write(download_response.content)
         return artifact['id']
     else:
         print('Failed to download artifact.')
 
-# Function to delete workflow run
+# Function to delete artifact
 def delete_artifact(artifact_id):
     url = f'https://api.github.com/repos/purs3lab/alpha-omega/actions/artifacts/{artifact_id}'
     response = requests.delete(url, headers=headers)
     if response.status_code == 204:
         print(f'Deleted artifact {artifact_id} successfully.')
+
+# Function to delete workflow run
+def delete_workflow_run(run_id):
+    url = f'https://api.github.com/repos/purs3lab/alpha-omega/actions/runs/{run_id}'
+    response = requests.delete(url, headers=headers)
+    if response.status_code == 204:
+        print(f'Deleted workflow run {run_id} successfully.')
+
+def delete_all_runs():
+    url = 'https://api.github.com/repos/purs3lab/alpha-omega/actions/runs'
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        runs = response.json()['workflow_runs']
+        for run in runs:
+            run_id = run['id']
+            delete_workflow_run(run_id)
 
 # Function to trigger the workflow
 def trigger_workflow(user, repo):
@@ -79,23 +115,37 @@ def validate_and_extract_github_url(url):
     match = github_url_pattern.match(url)
     return match.groups() if match else None
 
-# Main loop to run the workflow for each repository URL in the file
-count = 0
-with open(github_repos_file, 'r') as file:
-    for repo_url in file:
-        if args.limit > 0 and count >= args.limit:
-            break
-
-        repo_url = repo_url.strip()
-        if repo_url:
+def get_github_repos():
+    with open(github_repos_file, 'r') as file:
+        for repo_url in file:
+            repo_url = repo_url.strip()
+            if not repo_url:
+                continue
             result = validate_and_extract_github_url(repo_url)
-            if result:
-                user, repo = result
-                if trigger_workflow(user, repo):
-                    run_id = wait_for_workflow_completion(user, repo)
-                    artifact_id = download_artifact(run_id)
-                    if artifact_id:
-                        delete_artifact(artifact_id)
-                    count += 1
-            else:
+            if not result:
                 print(f'Invalid GitHub URL: {repo_url}')
+                continue
+            user, repo = result
+            if os.path.exists(f'{results_dir}/results-{user}-{repo}.zip'):
+                print(f'Skipped {user}/{repo}: result already exists')
+                continue
+            yield user, repo
+
+# Main loop to run the workflow for each repository URL in the file
+github_repos = get_github_repos()
+while True:
+    download_results()
+    nr_queued = num_of_queued_wf_runs()
+    if nr_queued != -1 and nr_queued < 1:
+        try:
+            user, repo = next(github_repos)
+            trigger_workflow(user, repo)
+        except StopIteration:
+            pass
+    time.sleep(60)  # Wait for 60 seconds before checking again
+
+#count = 0
+#with open(github_repos_file, 'r') as file:
+#    for repo_url in file:
+#        if args.limit > 0 and count >= args.limit:
+#            break
